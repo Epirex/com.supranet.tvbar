c022077c841fc0ce01e3f111b5e9e437d2127134
Epirex <estebanguzzo@gmail.com>
2024-07-31 17:30:29 -0300

    App fullscreen

--- app/src/main/java/com/supranet/tvbar/MainActivity.kt
+++ app/src/main/java/com/supranet/tvbar/MainActivity.kt
@@ -5,6 +5,7 @@ import android.hardware.usb.UsbDevice
 import android.os.Bundle
 import android.util.Log
 import android.view.Surface
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -22,6 +23,15 @@ class MainActivity : AppCompatActivity(), CameraDialog.CameraDialogParent {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Fullscreen
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+
         setContentView(R.layout.activity_main)
 
         mUSBMonitor = USBMonitor(this, mOnDeviceConnectListener)
